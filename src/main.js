6d80cf7409dde3451955ea0874659301265dd00d
陈斌 <178651286@qq.com>
2023-07-19 14:36:17 +0800

    1.0

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import directive from "./directive"
-const app=createApp(App)
+const app=createSSRApp(App)
 app.use(directive)
 app.mount('#app')
