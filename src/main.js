851b20c48c9ae0e4ca4caeffb40c0372690ec03a
zijid <1786512286@qq.com>
2023-09-26 19:11:55 +0800

    完善背景

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp,defineAsyncComponent } from 'vue'
 import App from './App.vue'
 import directive from "./directive"
+import "./main.css"
 const app=createApp(App)
 app.use(directive)
 
